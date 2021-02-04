b14fa85e799840523f93643772ebd1a1d9e32879
andyxu <andyxmq@126.com>
2021-02-04 19:53:06 +0800

    feat: 新增virtual组件

--- src/components/Virtual/virtual.stories.tsx
+++ src/components/Virtual/virtual.stories.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
+
 import Virtual, {IVirtualProps} from './virtual';
 
 export default {
   title: "Virtual",
-  component: Virtual
+  component: Virtual,
+  argTypes: { onSelect: { action: 'selected' } }
 } as Meta;
 let d:any[] = [];
 for (let i = 0; i < 1000; i++) {
   d.push({ id: i, value: i });
 }
 
-const Template: Story<IVirtualProps> = (args) => {
-  return <Virtual list={d} style={{
-    border: '1px solid #eee'
-  }}/>
+const style = {
+  border: '1px solid #eee'
 }
-
+const Template: Story<IVirtualProps> = (args) => {
+  return <Virtual {...args}/>
+}
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  data: d,
+  style,
+  screenHeight: 300,
+  itemSize: 30,
+};
--- src/components/Virtual/virtual.tsx
+++ src/components/Virtual/virtual.tsx
@@ -1,87 +1,109 @@
-import React, {FC, CSSProperties, useState, useRef, useEffect} from 'react';
+import React, {
+  FC,
+  CSSProperties,
+  useState,
+  useRef,
+  RefObject,
+  useEffect,
+  useCallback,
+  ReactElement
+} from 'react';
 import classnames from 'classnames';
-
-export interface IVirtualProps {
-  style?: CSSProperties,
-  className?: string,
-  list: any[]
+interface DataSourceObject {
+  value: string
 }
-const itemSize = 30;
-const bufferScale = 0.5;
+
+export type DataSourceType<T = {}> = T & DataSourceObject;
+export interface IVirtualProps {
+  style?: CSSProperties;
+  className?: string;
+  screenHeight: number;
+  itemSize: number;
+  data?: DataSourceType[];
+  bufferScale?: number;
+  onSelect?: (item: DataSourceType) => void;
+  renderOptions?: (item: DataSourceType) => ReactElement;
+}
+
 export const Virtual: FC<IVirtualProps> = props => {
   const {
     className,
-    list,
-    style
+    screenHeight,
+    itemSize,
+    data,
+    bufferScale,
+    style,
+    renderOptions,
+    onSelect
   } = props;
-  let [start, setStart] = useState<Number>(0);
-  let [end, setEnd] = useState<Number>(0);
+
+  let virtualContainerRef = useRef<HTMLDivElement>(null);
   let [visibleData, setVisibleData] = useState<any[]>([]);
-  let [listHeight, setListHeight] = useState<Number>(0);
-  let [startOffset, setStartOffset] = useState<Number>(0);
-  let virtual = useRef<HTMLDivElement>(null);
+  let [contentHeight, setContentHeight] = useState<number>(0);
+  let [startOffset, setStartOffset] = useState<number>(0);
+  // css样式处理
+  let containerStyle = Object.assign({height: `${screenHeight}px`}, style);
+  let containerClassName = classnames('virtual-container', className);
   useEffect(() => {
-    if(list.length && virtual.current){
-      // 列表总高度
-      let listHeight = list.length * itemSize;
-      let scrollTop = virtual.current.scrollTop;
-      let screenHeight = virtual.current.clientHeight;
-      let start = Math.floor(scrollTop / itemSize);
-      let visibleCount = Math.ceil(screenHeight / itemSize);
-      let above = Math.min(start, bufferScale * visibleCount);
-      let end = start + visibleCount;
-      let below = end +  bufferScale * visibleCount;
-      let visibleData = list.slice(start - above, start + visibleCount +below);
+    computeVisibleData(data!, virtualContainerRef);
+  }, [data, virtualContainerRef]);
+
+  const handleContainerScroll = useCallback(() => {
+    computeVisibleData(data!, virtualContainerRef);
+  }, [data, virtualContainerRef]);
+
+  const computeVisibleData = (data: any[], ref: RefObject<HTMLDivElement>) => {
+    if (data && data.length && ref.current) {
+      const contentHeight = data.length * itemSize;
+      const screenHeight = ref.current.clientHeight;
+      const scrollTop = ref.current.scrollTop;
+
+      const visibleCount = Math.ceil(screenHeight / itemSize);
+      const start = Math.floor(scrollTop / itemSize);
+      const end = start + visibleCount;
       
-      setStart(start);
-      setEnd(end);
-      setListHeight(listHeight);
-      setVisibleData(visibleData);
+      const above = Math.min(start, bufferScale! * visibleCount);
+      const below = Math.min(data.length - end, bufferScale! * visibleCount);
+      const visibleData = data.slice(start - above, end + below);
       
-      // console.log(listHeight, scrollTop, screenHeight, start, '===');
-    }
-  }, [list, virtual]);
-  const klass = classnames('virtual-container', className);
-  const handleScroll = () => {
-    if (virtual.current) {
-      let scrollTop = virtual.current.scrollTop;
-      let screenHeight = virtual.current.clientHeight;
-      let start = Math.floor(scrollTop / itemSize);
-      let visibleCount = Math.ceil(screenHeight / itemSize);
-      let startOffset = scrollTop - (scrollTop % itemSize);
-      let above = Math.min(start, bufferScale * visibleCount);
-      let end = start + visibleCount;
-      let below = end +  bufferScale * visibleCount;
-      let visibleData = list.slice(start + above, start + visibleCount + below);
-      setStart(start);
-      setEnd(end);
-      setStartOffset(startOffset)
       setVisibleData(visibleData);
-      // console.log(virtual.current.scrollTop);
+      setContentHeight(contentHeight);
+      setStartOffset(scrollTop - above * itemSize);
     }
+  } 
+  const handleSelect = (item: DataSourceType) => {
+    onSelect && onSelect(item);
   }
-  console.log(start, end);
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOptions ? renderOptions(item) : item.value;  
+  }
   return (
     <div 
-      ref={virtual}
-      style={style}
-      className={klass}
-      onScroll={handleScroll}
+      ref={virtualContainerRef}
+      style={containerStyle}
+      className={containerClassName}
+      onScroll={handleContainerScroll}
     >
-      <div className="virtual-phantom" style={{height: `${listHeight}px`}}></div>
-      <div className="virtual-list" style={{
+      <div className="virtual-phantom" style={{height: `${contentHeight}px`}}></div>
+      <ul className="virtual-list" style={{
         transform: `translate3d(0, ${startOffset}px, 0)`
       }}>
         {
-          visibleData.map(item => {
-            return <div key={item.key} className="virtual-list-item">{item.value}</div>
+          visibleData.map((item, index) => {
+            const c_name = classnames('virtual-item');
+            return (
+              <li key={index} onClick={() => handleSelect(item)} className={c_name}>
+                {renderTemplate(item)}
+              </li>
+            );
           })
         }
-      </div>
+      </ul>
     </div>
   );
 }
 Virtual.defaultProps = {
-  list: []
+  data: [],
+  bufferScale: 1
 };
 export default Virtual;
